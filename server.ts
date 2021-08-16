dc54a3e5339cb37764f197a64ff97726cc3a1513
wangzixi-diablo <cle.ee@163.com>
2021-08-16 11:19:16 +0800

    cannot find user login entry point!

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
-const ngExpressEngine = NgExpressEngineDecorator.get(engine);
+const ngExpressEngine = NgExpressEngineDecorator.get(engine, { timeout: 3000000, debug: true });
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
